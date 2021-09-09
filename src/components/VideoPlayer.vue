f8919adedf76defeae1d1e89394904c79707b5ad
Steven Almeroth <sroth77@gmail.com>
2021-09-09 12:28:07 +0200

    strip down and rename

---
+++ src/components/VideoPlayer.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  <button type="button" @click="count++">count is: {{ count }}</button>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
