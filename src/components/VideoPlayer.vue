77cce9209008271a62c9db912844049bf4970a75
Steven Almeroth <sroth77@gmail.com>
2021-09-09 22:37:39 +0200

    mark items

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -8,12 +8,14 @@ defineProps({
 })
 
 const time = ref(null)
+const items = ref([])
 const duration = ref(null)
 
 let player = null
 let interval = null
 
 function onPlayerPlay(event) {
+  console.log('onPlayerReady', this)
   if (this.duration === null) {
     this.duration = this.player.duration()
   }
@@ -33,6 +35,15 @@ export default {
         }
       }
       this.interval = window.setInterval(interval, 500)
+    },
+    mark() {
+      const items = this.items
+      const haveItems = !!items.length
+      const id = haveItems ? Math.max(...items.map(i => i.id)) + 1 : 0
+      this.items.push({
+        id,
+        time: this.player.currentTime(),
+      })
     },
   },
   mounted() {
@@ -60,6 +71,15 @@ export default {
       <div>{{ time }} / {{ parseInt(duration) }}</div>
     </div>
 
+    <div id="list-container" class="flecks">
+      <ul class="list">
+        <li v-for="item, i in items" :key="i">
+          <button v-text="item" class="clickable" />
+        </li>
+      </ul>
+      <button class="clickable" @click="mark"><strong> + </strong></button>
+    </div>
+
   </div>
 </template>
 
@@ -74,5 +94,14 @@ export default {
     vertical-align: top;
     padding: 10px;
     margin:  10px;
+  }
+  .list {
+    list-style-type: none;
+    padding-inline-start: 0;
+    text-align: left;
+    margin-block-start: 0;
+  }
+  .clickable {
+    cursor: pointer;
   }
 </style>
