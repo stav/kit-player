a4727a03998c9fab99a7867fe3196b16f7138ef1
Steven Almeroth <sroth77@gmail.com>
2021-09-10 08:17:52 +0200

    cue item

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -7,6 +7,7 @@ defineProps({
   options: Object,
 })
 
+const id = ref(null)
 const time = ref(null)
 const items = ref([])
 const duration = ref(null)
@@ -15,21 +16,33 @@ let player = null
 let interval = null
 
 function onPlayerPlay(event) {
-  console.log('onPlayerReady', this)
   if (this.duration === null) {
     this.duration = this.player.duration()
   }
 }
+
+function cue(id) {
+  this.id = this.id === id ? null : id
+  const item = this.items.find( i => i.id === id )
+  this.time = parseInt(item.time)
+  this.player.currentTime(item.time)
+}
+
 </script>
 
 <script>
 export default {
   computed: {
     itemsSortedByTime() {
-      console.log('itemsSortedByTime', this)
       const items = [...this.items]
       items.sort((a, b) => a.time - b.time)
       return items
+    },
+    cuedItem() {
+      return this.items.find(item => item.id === this.id)
+    },
+    cuedItemIndex() {
+      return this.items.findIndex(item => item.id === this.id)
     },
   },
   methods: {
@@ -53,6 +66,10 @@ export default {
         time: this.player.currentTime(),
       })
     },
+   removeCuedItem() {
+      this.items.splice(this.cuedItemIndex, 1)
+      this.id = null
+    }
   },
   mounted() {
     this.player = videojs(this.$refs.videoPlayer, this.options, this.onPlayerReady)
@@ -82,10 +99,20 @@ export default {
     <div id="list-container" class="flecks">
       <ul class="list">
         <li v-for="item, i in itemsSortedByTime" :key="i">
-          <button v-text="item" class="clickable" />
+          <button v-text="item" class="clickable" @click="() => cue(item.id)" />
         </li>
       </ul>
       <button class="clickable" @click="mark"><strong> + </strong></button>
+    </div>
+
+    <div id="form-container" class="flecks">
+      <div v-if="id !== null">
+        {{ id }}
+        <button class="clickable" @click="removeCuedItem" title="Remove this item"> X </button>
+        {{ cuedItem }}
+        time <input type="text" name="time" v-model="cuedItem.time" />
+        text <input type="text" name="text" v-model="cuedItem.text" />
+      </div>
     </div>
 
   </div>
@@ -94,6 +121,12 @@ export default {
 <style scoped>
   #video-container {
     border-color: red;
+  }
+  #list-container {
+    border-color: blue;
+  }
+  #form-container {
+    border-color: green;
   }
   .flecks {
     display: inline-block;
