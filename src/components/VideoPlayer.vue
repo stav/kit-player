b72cea4ede39c2f0f31d95e520774d3110960214
Steven Almeroth <sroth77@gmail.com>
2021-09-11 20:46:02 +0200

    display item description

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -10,6 +10,7 @@ defineProps({
 
 const id = ref(null)
 const items = ref([])
+const description = ref(true)
 const timeCurrent = ref(null)
 const timeBuffered = ref(null)
 const timeDuration = ref(null)
@@ -186,12 +187,15 @@ export default {
     <div id="list-container" class="flecks">
       <ul class="list">
         <li v-for="item, i in itemsSortedByTime" :key="i">
-          <button v-text="item" class="clickable" @click="($event) => cue(item.id, $event)" />
+          <button v-text="description ? item.text ? item.text : item : item" class="clickable" @click="($event) => cue(item.id, $event)" />
         </li>
       </ul>
       <div class="kit-flex">
         <div class="kit-cell">
           <button class="clickable" @click="mark" title="Mark cue point now"><strong> + </strong></button>
+        </div>
+        <div class="kit-cell" align="center">
+          <button class="clickable" @click="description = !description" title="Toggle display"><strong> o </strong></button>
         </div>
         <div class="kit-cell" align="right">
           <button class="clickable" @click="removeAllItems" title="Remove ALL cue marks!!"><strong> X </strong></button>
@@ -252,7 +256,7 @@ export default {
     display: flex;
   }
   .kit-cell {
-    width: 50%;
+    width: 33%;
     display: table-cell;
   }
 </style>
